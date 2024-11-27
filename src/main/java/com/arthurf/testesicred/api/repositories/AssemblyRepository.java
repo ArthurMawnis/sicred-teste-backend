c8454dc12e4810178eba27947227c9d44e78e75f
Arthur Freitas <arthur.cdfreitas@gmail.com>
2024-11-27 18:05:58 -0300

    feature: added AssemblyRepository

---
+++ src/main/java/com/arthurf/testesicred/api/repositories/AssemblyRepository.java
@@ -0,0 +1,13 @@
+package com.arthurf.testesicred.api.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.arthurf.testesicred.api.models.Assembly;
+
+@Repository
+public interface AssemblyRepository extends MongoRepository<Assembly, UUID> {
+
+}
