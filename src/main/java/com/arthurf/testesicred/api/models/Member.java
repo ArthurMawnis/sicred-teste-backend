b5b3372e8b2ca99093520c103914cf4e3c13fab8
Arthur Freitas <arthur.cdfreitas@gmail.com>
2024-11-28 20:40:29 -0300

    feature: adding the attribute cpf to Members

--- src/main/java/com/arthurf/testesicred/api/models/Member.java
+++ src/main/java/com/arthurf/testesicred/api/models/Member.java
@@ -1,16 +1,16 @@
 package com.arthurf.testesicred.api.models;
 
-import java.io.Serializable;
 import java.util.UUID;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "members")
-public class Member implements Serializable {
+public class Member {
 
     @Id()
     private UUID id;
+    private String cpf;
 
     public UUID getId() {
         return id;
@@ -18,5 +18,13 @@ public class Member implements Serializable {
 
     public void setId(UUID id) {
         this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 }
