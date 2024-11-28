fcb7be943df60409ce934d278a31f16967f68bd0
Arthur Freitas <arthur.cdfreitas@gmail.com>
2024-11-28 16:06:32 -0300

    feature: adding agenda's repository query: find by subject

--- src/main/java/com/arthurf/testesicred/api/repositories/AgendaRepository.java
+++ src/main/java/com/arthurf/testesicred/api/repositories/AgendaRepository.java
@@ -1,5 +1,6 @@
 package com.arthurf.testesicred.api.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,4 +10,13 @@ import com.arthurf.testesicred.api.models.Agenda;
 
 @Repository
 public interface AgendaRepository extends MongoRepository<Agenda, UUID> {
+
+    /**
+     * Find an agenda by subject.
+     * 
+     * @param subject the subject.
+     * @return the agenda.
+     */
+    Optional<Agenda> findBySubject(final String subject);
+
 }
