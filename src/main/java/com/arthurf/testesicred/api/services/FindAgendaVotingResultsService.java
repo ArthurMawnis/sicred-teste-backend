d39d9a24c462146c447d909cf7c3de56237f05fa
Arthur Freitas <arthur.cdfreitas@gmail.com>
2024-11-29 02:38:04 -0300

    featyre: adding endpoint for agenda voting results query

---
+++ src/main/java/com/arthurf/testesicred/api/services/FindAgendaVotingResultsService.java
@@ -0,0 +1,37 @@
+package com.arthurf.testesicred.api.services;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import com.arthurf.testesicred.api.exceptions.BusinessException;
+import com.arthurf.testesicred.api.models.AgendaVotingResult;
+import com.arthurf.testesicred.api.repositories.AgendaVotingResultRepository;
+import com.arthurf.testesicred.api.utils.BusinessUtils;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class FindAgendaVotingResultsService {
+
+    @Autowired
+    private AgendaVotingResultRepository agendaVotingResultRepository;
+
+    /**
+     * Find the voting results of an agenda
+     * 
+     * @param id The agenda identifier
+     * @return The voting results
+     */
+    public Mono<AgendaVotingResult> execute(String id) {
+        if (!BusinessUtils.isUUID(id)) {
+            throw new BusinessException("Invalid agenda identifier.", HttpStatus.BAD_REQUEST);
+        }
+
+        return Mono.just(agendaVotingResultRepository.findByAgendaId(UUID.fromString(id)).orElseThrow(
+                () -> new BusinessException("There's are no results for this agenda.", HttpStatus.NOT_FOUND)));
+    }
+
+}
