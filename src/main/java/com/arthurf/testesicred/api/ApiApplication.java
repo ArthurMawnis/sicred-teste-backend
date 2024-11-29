72fc9fa81c66dddda488836ea553eb89d8d0ea36
Arthur Freitas <arthur.cdfreitas@gmail.com>
2024-11-29 01:29:07 -0300

    feature: enabling spring scheduling

--- src/main/java/com/arthurf/testesicred/api/ApiApplication.java
+++ src/main/java/com/arthurf/testesicred/api/ApiApplication.java
@@ -2,8 +2,12 @@ package com.arthurf.testesicred.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableMongoAuditing
 public class ApiApplication {
 
 	public static void main(String[] args) {
