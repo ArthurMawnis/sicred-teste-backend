056b43893f1fe8eaf904ee8d4b2560db50e8d4b9
Arthur Freitas <arthur.cdfreitas@gmail.com>
2024-11-30 18:42:12 -0300

    fix: voteRepository#countByIdAgendaIdAndIsPositive would not fetch data correctly due to type difference between UUID and string

--- src/main/java/com/arthurf/testesicred/api/repositories/VoteRepository.java
+++ src/main/java/com/arthurf/testesicred/api/repositories/VoteRepository.java
@@ -1,8 +1,7 @@
 package com.arthurf.testesicred.api.repositories;
 
-import java.util.UUID;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.arthurf.testesicred.api.models.Vote;
@@ -11,4 +10,6 @@ import com.arthurf.testesicred.api.models.pks.VotePk;
 @Repository
 public interface VoteRepository extends MongoRepository<Vote, VotePk> {
 
-    Long countByIdAgendaIdAndIsPositive(UUID id, boolean b);}
+    @Query(value = "{ 'id.agendaId' : ?0, 'isPositive' : ?1 }", count = true)
+    Long countByIdAgendaIdAndIsPositive(final String id, boolean b);
+}
